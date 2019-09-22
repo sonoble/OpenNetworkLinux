bba9394dfb9c96f0ed9f3888eda7e6984ce5de70
Steven Noble <snoble@sonn.com>
2019-09-22 10:29:01 -0700

    Update build.sh
    disable automatic change to master branch for now

--- tools/autobuild/build.sh
+++ tools/autobuild/build.sh
@@ -81,8 +81,8 @@ echo "Now running under $DOCKER_IMAGE..."
 # The expectation is that we will already be on the required branch.
 # This is to normalize environments where the checkout might instead
 # be in a detached head (like jenkins)
-echo "Switching to branch $BUILD_BRANCH..."
-cd $ONL && git checkout $BUILD_BRANCH
+# echo "Switching to branch $BUILD_BRANCH..."
+# cd $ONL && git checkout $BUILD_BRANCH
 
 
 #
