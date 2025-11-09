59c1b4157b400da7375d34df0cc74e236ff7a9f1
Steven Noble <snoble@sonn.com>
2025-11-09 07:39:37 +0000

    fix: add i2c_smbus inline function definitions for STX60D0-126F
    Add inline implementations of i2c_smbus_* functions to platform_lib.h
    to resolve implicit function declaration errors during compilation.
    
    The kernel's <linux/i2c-dev.h> header only provides structure definitions
    and ioctl constants, but does not include the i2c_smbus_* helper functions.
    These functions are typically provided by the i2c-tools package's version
    of i2c-dev.h as inline functions.
    
    Since the build environment may not have i2c-tools headers, we provide
    the inline function implementations directly in platform_lib.h. These
    implementations are standard and come from the i2c-tools project.
    
    Functions added:
    - i2c_smbus_access() - Core ioctl wrapper for SMBus operations
    - i2c_smbus_read_byte_data() - Read single byte from register
    - i2c_smbus_write_byte_data() - Write single byte to register
    - i2c_smbus_read_byte() - Read byte without register
    - i2c_smbus_read_word_data() - Read 16-bit word from register
    - i2c_smbus_read_i2c_block_data() - Read block of data from register
    
    All functions use the underlying ioctl(I2C_SMBUS) system call with
    appropriate i2c_smbus_ioctl_data structures.
    
    Resolves compilation errors:
    - implicit declaration of function 'i2c_smbus_read_byte_data'
    - implicit declaration of function 'i2c_smbus_write_byte_data'
    - implicit declaration of function 'i2c_smbus_read_byte'
    - implicit declaration of function 'i2c_smbus_read_word_data'
    - implicit declaration of function 'i2c_smbus_read_i2c_block_data'

--- packages/platforms/alphanetworks/x86-64/stx60d0-126f/onlp/builds/x86_64_alphanetworks_stx60d0_126f/module/src/platform_lib.h
+++ packages/platforms/alphanetworks/x86-64/stx60d0-126f/onlp/builds/x86_64_alphanetworks_stx60d0_126f/module/src/platform_lib.h
@@ -248,6 +248,78 @@ i2c APIs: access i2c device by ioctl
 #include <linux/i2c.h>
 #endif
 
+/*
+ * i2c_smbus inline functions from i2c-tools i2c-dev.h
+ * These are not provided by the kernel's i2c-dev.h, so we define them here.
+ */
+static inline __s32 i2c_smbus_access(int file, char read_write, __u8 command,
+                                     int size, union i2c_smbus_data *data)
+{
+    struct i2c_smbus_ioctl_data args;
+    args.read_write = read_write;
+    args.command = command;
+    args.size = size;
+    args.data = data;
+    return ioctl(file, I2C_SMBUS, &args);
+}
+
+static inline __s32 i2c_smbus_read_byte_data(int file, __u8 command)
+{
+    union i2c_smbus_data data;
+    if (i2c_smbus_access(file, I2C_SMBUS_READ, command,
+                         I2C_SMBUS_BYTE_DATA, &data))
+        return -1;
+    else
+        return 0x0FF & data.byte;
+}
+
+static inline __s32 i2c_smbus_write_byte_data(int file, __u8 command, __u8 value)
+{
+    union i2c_smbus_data data;
+    data.byte = value;
+    return i2c_smbus_access(file, I2C_SMBUS_WRITE, command,
+                            I2C_SMBUS_BYTE_DATA, &data);
+}
+
+static inline __s32 i2c_smbus_read_byte(int file)
+{
+    union i2c_smbus_data data;
+    if (i2c_smbus_access(file, I2C_SMBUS_READ, 0,
+                         I2C_SMBUS_BYTE, &data))
+        return -1;
+    else
+        return 0x0FF & data.byte;
+}
+
+static inline __s32 i2c_smbus_read_word_data(int file, __u8 command)
+{
+    union i2c_smbus_data data;
+    if (i2c_smbus_access(file, I2C_SMBUS_READ, command,
+                         I2C_SMBUS_WORD_DATA, &data))
+        return -1;
+    else
+        return 0x0FFFF & data.word;
+}
+
+static inline __s32 i2c_smbus_read_i2c_block_data(int file, __u8 command,
+                                                   __u8 length, __u8 *values)
+{
+    union i2c_smbus_data data;
+    int i;
+    if (length > I2C_SMBUS_BLOCK_MAX)
+        length = I2C_SMBUS_BLOCK_MAX;
+    data.block[0] = length;
+    if (i2c_smbus_access(file, I2C_SMBUS_READ, command,
+                         length == 32 ? I2C_SMBUS_I2C_BLOCK_BROKEN :
+                          I2C_SMBUS_I2C_BLOCK_DATA, &data))
+        return -1;
+    else {
+        for (i = 1; i <= data.block[0]; i++)
+            values[i-1] = data.block[i];
+        return data.block[0];
+    }
+}
+
 int i2c_read(int i2cbus, int addr, int offset, int length, char* data);
 int i2c_sequential_read(int i2cbus, int addr, int offset, int length, char* data);
 int i2c_read_byte(int i2cbus, int addr, int offset, char* data);
